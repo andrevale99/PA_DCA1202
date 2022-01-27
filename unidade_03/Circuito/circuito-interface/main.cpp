a5cae00977abca944d26faa099f6d38a4d81de2a
andrevale99 <andrefvale@gmail.com>
2022-01-27 09:47:22 -0300

    comentario da dupla

--- unidade_03/Circuito/circuito-interface/main.cpp
+++ unidade_03/Circuito/circuito-interface/main.cpp
@@ -1,6 +1,12 @@
 #include "maincircuito.h"
 #include <QApplication>
 
+/**
+ * @Dupla
+ * 		André Menezes de Freitas Vale
+ * 		Lucas Emanuel Costa Moisés
+*/
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
